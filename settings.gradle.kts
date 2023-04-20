dd227fe5a0b6c66f4340480b6adf95cfd9c7bd8e
Nico <jumpingpxl@gmail.com>
2023-04-20 16:59:10 +0200

    that was the wrong labygradle version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "resourcepackstwentyfour"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.3.22"
+    val labyGradlePluginVersion = "0.3.21"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
